b083427f2a2e13ed702d8bc08e7124de4d2849a8
lomirus <lomirus@qq.com>
2021-12-17 21:35:19 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -29,9 +29,23 @@ impl Plugin for HelloPlugin {
     }
 }
 
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let texture_handle = asset_server.load::<Texture, &'static str>("image.jpg");
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(texture_handle.into()),
+        ..Default::default()
+    });
+}
+
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_plugin(HelloPlugin)
+        .add_startup_system(setup.system())
         .run();
 }
